4bd7a6b8edcd35926479034b5e81894125edc9bf
marekgorskiwsb@gmail.com <gorskimarekgm@gmail.com>
2020-11-20 19:31:19 +0100

    post model added

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    likes = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="author")
+
+
+
